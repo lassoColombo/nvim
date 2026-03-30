972c955e00a494d5b5a89e9ba047b99797660dcd
lasso <lasso.colombo@gmail.com>
2026-03-30 22:32:10 +0200

    fix: disable json schemastore

--- lua/plugs/lsp/servers/jsonls.lua
+++ lua/plugs/lsp/servers/jsonls.lua
@@ -3,6 +3,7 @@ return {
     json = {
       format = { enable = true },
       validate = { enable = true },
+      schemaStore = { enable = false, url = '' },
       schemas = require('schemastore').json.schemas(),
     },
   },
